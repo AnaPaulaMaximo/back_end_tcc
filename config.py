0f56882ca56862af707576ae1c4207f707f65992
AnaPaulaMaximo <ana.maximo.senaisp@gmail.com>
2025-05-30 12:22:50 -0300

    retirando banco

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
-import mysql.connector
+# import mysql.connector
 
-ambiente = 'desenvolvimento'
+# ambiente = 'desenvolvimento'
 
-if ambiente == 'desenvolvimento':
-    DB_HOST = 'localhost'
-    DB_USER = 'root'
-    DB_PASSWORD = 'senai'
-    DB_NAME = 'teste_usuarios'
+# if ambiente == 'desenvolvimento':
+#     DB_HOST = 'localhost'
+#     DB_USER = 'root'
+#     DB_PASSWORD = 'senai'
+#     DB_NAME = 'teste_usuarios'
 
-conn = mysql.connector.connect(
-    host=DB_HOST,
-    user=DB_USER,
-    password=DB_PASSWORD,
-    database=DB_NAME
-)
-cursor = conn.cursor(dictionary=True)
+# conn = mysql.connector.connect(
+#     host=DB_HOST,
+#     user=DB_USER,
+#     password=DB_PASSWORD,
+#     database=DB_NAME
+# )
+# cursor = conn.cursor(dictionary=True)
